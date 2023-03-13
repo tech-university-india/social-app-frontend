779a832cc381239169e8eb8efbaf33a2463a3527
Darshit Dave <darshitdave02@gmail.com>
2023-03-13 17:29:26 +0530

    chore: structure change

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Navbar } from '../../components';
 import ProfileInfo from '../../components/ProfileInfo';
 
 function ProfilePage() {
     return (
         <div>
+            <Navbar />
             <ProfileInfo />
+            
         </div>
     );
 }
