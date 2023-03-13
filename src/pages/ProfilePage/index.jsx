51aa3b1bd846691bb98239db4b690ea948a27c3a
Darshit Dave <darshitdave02@gmail.com>
2023-03-13 15:45:57 +0530

    fix: minor errors in config

---
+++ src/pages/ProfilePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileInfo from '../../components/ProfileInfo';
+
+function ProfilePage() {
+    return (
+        <div>
+            <ProfileInfo />
+        </div>
+    );
+}
+
+export default ProfilePage;
